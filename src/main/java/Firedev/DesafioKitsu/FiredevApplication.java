245ff63b8dc45a3f9fcba00c65fd1769fa76a67c
Elbert Lucas <elbertlucas0@gmail.com>
2022-01-06 14:00:34 -0300

    Spring init

---
+++ src/main/java/Firedev/DesafioKitsu/FiredevApplication.java
@@ -0,0 +1,13 @@
+package Firedev.DesafioKitsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiredevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiredevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Firedev/DesafioKitsu/FiredevApplicationTests.java
@@ -0,0 +1,13 @@
+package Firedev.DesafioKitsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiredevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
